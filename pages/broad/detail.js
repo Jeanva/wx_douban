9dbec0aeaa2b9fb1b5aac0e5c852cc3a3438304e
jeanva <591975433@qq.com>
2018-06-05 19:12:05 +0800

    commit all file

--- pages/broad/detail.js
+++ pages/broad/detail.js
@@ -8,13 +8,59 @@ Page({
     title:'广播详情',
     po_date:'04-01',
     new_likes:15,
+    currentTab:0,
+    comments:[
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'喜羊羊',
+        date:'5天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'灰太狼',
+        date:'6天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'熊大',
+        date:'7天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'熊二',
+        date:'8天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'',
+        date:'9天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'',
+        date:'9天前'
+      },
+      {
+        user:'风轻云淡',
+        comment:'听过他的CD，从此不能自拔',
+        to:'',
+        date:'9天前'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+
   },
 
   /**
@@ -64,5 +110,27 @@ Page({
    */
   onShareAppMessage: function () {
   
-  }
+  },
+  
+
+  //滑动切换
+  swiperTab:function( e ){
+    var that=this;
+    that.setData({
+      currentTba:e.detail.current
+    });
+  },
+  //点击切换
+  clickTab: function( e ) {  
+
+    var that = this;  
+
+    if( this.data.currentTab === e.target.dataset.current ) {  
+      return false;  
+    } else {  
+      that.setData( {  
+        currentTab: e.target.dataset.current  
+      })  
+    }  
+  }  
 })
