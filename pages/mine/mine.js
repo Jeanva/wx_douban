cfc4a532f62adab639436e1d552b8582fa5a2fe7
jeanva <591975433@qq.com>
2018-06-21 23:31:26 +0800

    用户详情页功能完善

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -7,36 +7,60 @@ Page({
   data: {
       tagList:[
         {
+          "tag":"扫一扫",
+          "imgUrl":"/image/sys.png",
+          "url":"",
+          'itemFn':'scan',
+        },
+        {
         "tag":"喜欢",
         "imgUrl":"/image/heart.png",
+        "url":'',
+
         },
         {
         "tag":"日记",
         "imgUrl":"/image/dairy.png",
+        "url":'/pages/broad/detail',
+        'itemFn':'toDairy',
         },
         {
           "tag":"相册",
           "imgUrl":"/image/pics.png",
+          "url":'',
+          "itemFn":'toAlbum'
         },
         {
           "tag":"我的关注",
           "imgUrl":"/image/add.png",
+          "url":'',
+          "itemFn":'',
         },
         {
           "tag":"电影.电视.综艺",
           "imgUrl":"/image/video.png",
+          "url":'',
+          "itemFn":'toBroad',
         },
         {
           "tag":"读书",
           "imgUrl":"/image/mybook.png",
+          "url":''
         },
         {
           "tag":"音乐",
           "imgUrl":"/image/music.png",
+          "url":''
         },
         {
           "tag":"钱包",
           "imgUrl":"/image/wallet.png",
+          "url":''
+        },
+        {
+          'tag':'地图',
+          'imgUrl':'/image/pics.png',
+          'url':'',
         }
     ]
   },
@@ -95,5 +119,63 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  gotoalbum:function(){
+    wx.navigateTo({
+      url: '/pages/album/album',
+    })
+  },
+  //去书影音
+  toBroad:function(e){
+    wx.navigateTo({
+      url:'../../pages/broad/detail',
+    });
+    console.log(e);
+    console.log(data.tagList);
+  },
+  //扫一扫
+  scan: function () {
+    var that = this;
+    var show;
+    wx.scanCode({
+      success: (res) => {
+        this.show = "--result:" + res.result + "--scanType:" + res.scanType + "--charSet:" + res.charSet + "--path:" + res.path;
+        that.setData({
+          show: this.show
+        })
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '失败',
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      complete: (res) => {
+      }
+    })
+  },
+  // tonavigator:function(e){
+  //   console.log(e.data.tagList.item.url);
+  //   wx.navigateTo({
+  //     url: e.data.tagList.item.url,
+  //   })
+  // }
+  // 去日记详情页
+  toDairy:function(){
+    wx.navigateTo({
+      url: '../../pages/dairy/dairy-list',
+    })
+  },
+  //去相册
+  toAlbum:function(){
+    wx.navigateTo({
+      url:'../../pages/album/album',
+    });
   }
 })
